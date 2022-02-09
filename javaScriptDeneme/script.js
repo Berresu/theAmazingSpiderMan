689f1a26257a13003acd496522094c51cb8af3cc
Berresu <90646787+Berresu@users.noreply.github.com>
2022-02-09 22:58:04 +0300

    Add files via upload

---
+++ javaScriptDeneme/script.js
@@ -0,0 +1,7 @@
+function renkDegistirin(){
+    document.body.style.backgroundColor="blue";
+    document.body.style.color="red";
+    document.body.style.fontSize="2rem";
+    document.getElementById("theAmazing").setAttribute("src" , "The Amazing Spider-Man.gif");
+    document.getElementById("spidey").innerHTML="The Amazing Spider-Man"
+}
